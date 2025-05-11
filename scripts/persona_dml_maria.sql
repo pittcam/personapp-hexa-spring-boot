c805ae809436eb78ccb49ed2869aa0a88dfb033c
Camila Neiza <castronjulianac@javeriana.edu.co>
2025-05-11 17:25:30 -0500

    docker funcional

--- scripts/persona_dml_maria.sql
+++ scripts/persona_dml_maria.sql
@@ -6,3 +6,21 @@ VALUES
 	(321654987,'Pepa','Juarez','F',30),
 	(147258369,'Pepita','Juarez','F',10),
 	(963852741,'Fede','Perez','M',18);
+
+	-- Insertar profesiones
+INSERT INTO `persona_db`.`profesion` (`id`, `nom`, `des`) VALUES
+  (1, 'Ingeniero', 'Diseña soluciones técnicas'),
+  (2, 'Médico', 'Especialista en salud'),
+  (3, 'Profesor', 'Educador profesional');
+
+-- Insertar teléfonos
+INSERT INTO `persona_db`.`telefono` (`num`, `oper`, `duenio`) VALUES
+  ('3001234567', 'Claro', 123456789),
+  ('3107654321', 'Movistar', 987654321),
+  ('3134567890', 'Tigo', 321654987);
+
+-- Insertar estudios
+INSERT INTO `persona_db`.`estudios` (`id_prof`, `cc_per`, `fecha`, `univer`) VALUES
+  (1, 123456789, '2015-06-10', 'Javeriana'),
+  (2, 987654321, '2020-08-15', 'Nacional'),
+  (3, 321654987, '2012-02-20', 'Andes');
