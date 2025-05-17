c05413cda8ec745af790b3992da7d85092bd6efe
Juanba2002 <bajuan00@gmail.com>
2025-05-17 13:34:43 -0500

    Cambio MOGNO

--- scripts/persona_ddl_mongo.js
+++ scripts/persona_ddl_mongo.js
@@ -1,11 +1,12 @@
 use admin
 
-db.createUser({
+db.getSiblingDB('admin').createUser({
   user: "persona_db",
   pwd: "persona_db",
   roles: [
     { role: "readWrite", db: "persona_db" },
     { role: "dbAdmin", db: "persona_db" }
   ],
-  mechanisms: ["SCRAM-SHA-1","SCRAM-SHA-256"]
-})
+  mechanisms: ["SCRAM-SHA-1", "SCRAM-SHA-256"]
+});
+
